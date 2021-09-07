9e05e8d118bcf8bbecb9d76c46e3a3def3a9e617
pareshponline <pareshponline@hotmail.com>
2021-09-07 08:12:29 +0530

    1 file modified, 4 added

---
+++ src/app/hellochild/hellochild.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hellochild',
+  templateUrl: './hellochild.component.html',
+  styleUrls: ['./hellochild.component.css']
+})
+export class HellochildComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
